68ded680393af575ebca7f70c9cf56a8eb37c012
WeiYC <weiyu1213@gmail.com>
2024-12-08 03:55:58 +0000

    1208_04

--- Lesson1/01_Hello.py
+++ Lesson1/01_Hello.py
@@ -1,2 +1,15 @@
+'''
 print("Hell World")
 print("Hi")
+'''
+
+
+#設定變數 存放學校名稱 學生名稱 學生電話 並列印出來
+
+school_Name = '健行科技大學' #<----駝峰是命名（camel case）
+student_Name = '小明'
+student_Phone = '0912-345678'
+
+print(f"學校名稱：{school_Name}")
+print(f"學生名稱：{student_Name}")
+print(f"學生電話：{student_Phone}")
