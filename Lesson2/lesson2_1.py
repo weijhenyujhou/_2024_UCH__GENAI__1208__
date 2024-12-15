57347bfb2b7b6bfa769130b95434a33d4eac5e76
WeiYC <weiyu1213@gmail.com>
2024-12-15 03:59:44 +0000

    1215_03_MB

--- Lesson2/lesson2_1.py
+++ Lesson2/lesson2_1.py
@@ -1,8 +1,3 @@
-# 台幣轉美金函數
-def currency_converter(TWD_dollar):
-    exchange_rate = 0.035
-    USdollar = TWD_dollar*exchange_rate
-    return USdollar
+import tools
 
-
-currency_converter(1000)
+tools.幣值轉換()
